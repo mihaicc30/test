a77e7993d3f848cc057de71b9d7c2d15c26ea013
mihaicc30 <alemihai25@gmail.com>
2023-12-20 21:48:04 +0000

    uploaded test

--- App.tsx
+++ App.tsx
@@ -1,11 +1,23 @@
-import React, { useState } from "react";
-import { Button, View, Text, Image } from "react-native";
+import React from "react";
 import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import { HomeScreen } from "./components/HomeScreen";
+import { LoginScreen } from "./components/LoginScreen";
+import { DashboardScreen } from "./components/DashboardScreen";
 
 const Stack = createNativeStackNavigator();
 
-type User = string | null
+export type User = {
+  name: string;
+  email: string;
+  password: string;
+};
+
+export type Errors = {
+  name: string;
+  email: string;
+  password: string;
+};
 
 function App() {
   return (
@@ -18,7 +30,6 @@ function App() {
       >
         <Stack.Screen name="Home" component={HomeScreen} />
         <Stack.Screen name="Login" component={LoginScreen} />
-        <Stack.Screen name="Register" component={RegisterScreen} />
         <Stack.Screen name="Dashboard" component={DashboardScreen} />
       </Stack.Navigator>
     </NavigationContainer>
@@ -26,47 +37,3 @@ function App() {
 }
 
 export default App;
-
-function HomeScreen({ route, navigation }) {
-  const [user, setUser] = useState<User>(null);
-
-  const dothis = () => {
-    navigation.navigate("Login", {
-      user: user,
-    });
-  };
-
-  return (
-    <View className="flex-1 items-center justify-center bg-blue-400">
-      {/* <Image source={require('./10z.PNG')}  /> */}
-      <Text>Open up Aaaaaaapp.tsx to start working on your app!</Text>
-      <Button title="Go to Login" onPress={dothis} />
-    </View>
-  );
-}
-
-function LoginScreen({ route, navigation }) {
-  const { user } = route.params;
-  return (
-    <View className="flex-1 items-center justify-center bg-blue-400">
-      <Text>hi {route.params.user.name}</Text>
-
-      <Button title="Go back" onPress={() => navigation.goBack()} />
-    </View>
-  );
-}
-
-function RegisterScreen({ route, navigation }) {
-  return (
-    <View className="flex-1 items-center justify-center bg-blue-400">
-      <Text>Open up Aaaaaaapp.tsx to start working on your app!</Text>
-    </View>
-  );
-}
-function DashboardScreen({ route, navigation }) {
-  return (
-    <View className="flex-1 items-center justify-center bg-blue-400">
-      <Text>Open up Aaaaaaapp.tsx to start working on your app!</Text>
-    </View>
-  );
-}
